8a97c7f8b6ee6b8cfa7324cae3b1afde487afb9f
Darshanghale07 <darshanghale07@gmail.com>
2024-05-28 18:02:43 +0545

    get,post api for patient added

--- HMS-I/routes/api.php
+++ HMS-I/routes/api.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\PatientApiController;
 
+
+
+use App\Http\Controllers\DocController;
+use App\Http\Controllers\PatientController;
+use App\Http\Controllers\NurseController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get("index",[PatientController::class,'index']);
+Route::get("paview",[PatientApiController::class,'paview']);
+Route::get("paview-detail/{id}",[PatientApiController::class,'paviewed']);    
+Route::post("paregister",[PatientApiController::class,'pastore']);  
--- HMS-I/routes/web.php
+++ HMS-I/routes/web.php
@@ -18,7 +18,7 @@ route::get('/', [PatientController::class,'index'])->name('pa.home');
 route::get('/auth',[AuthController::class,'authlogin'])->name('authlogin');
 route::post('/auth',[AuthController::class,'authlogina'])->name('authlogina');
 
-Route::group(['middleware'=> ['Patient']], function () {
+//Route::group(['middleware'=> ['Patient']], function () {});
     
 
 route::get('/patient',[PatientController::class,'paform'])->name('pa.add');
@@ -28,9 +28,9 @@ route::get('/patient/view', [PatientController::class,'paview'])->name('pa.view'
 route::get('/patient/edit/{id}',[PatientController::class,'paedit'])->name('pa.edit'); 
 route::post('/patient/update/{id}',[PatientController::class,'paupdate'])->name('pa.update'); 
 route::get('/patient/delete/{id}',[PatientController::class,'padelete'])->name('pa.delete'); 
-});
 
-Route::group(['middleware' => ['Doctor']], function () {
+
+//Route::group(['middleware' => ['Doctor']], function () {});
     
 
 route::get('/doctor',[DocController::class,'dform'])->name('dc.add');
@@ -40,9 +40,9 @@ route::get('/doctor/view', [DocController::class,'dview'])->name('dc.view');
 route::get('/doctor/edit/{id}',[DocController::class,'dedit'])->name('dc.edit'); 
 route::post('/doctor/update/{id}',[DocController::class,'dupdate'])->name('dc.update'); 
 route::get('/doctor/delete/{id}',[DocController::class,'ddelete'])->name('dc.delete'); 
-});
 
-Route::group(['middleware' => ['Nurse']], function () {
+
+//Route::group(['middleware' => ['Nurse']], function () {});
     
 
 route::get('/nurse',[NurseController::class,'nform'])->name('nr.add');
@@ -52,4 +52,3 @@ route::get('/nurse/view', [NurseController::class,'nview'])->name('nr.view');
 route::get('/nurse/edit/{id}',[NurseController::class,'nedit'])->name('nr.edit'); 
 route::post('/nurse/update/{id}',[NurseController::class,'nupdate'])->name('nr.update'); 
 route::get('/nurse/delete/{id}',[NurseController::class,'ndelete'])->name('nr.delete');
-});
